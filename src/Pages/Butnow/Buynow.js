1dc3859949d07cd56bbcff75ebe14852025cfb92
BismaAsif <bismaasif274@gmail.com>
2021-03-29 14:28:44 +0500

    playstore and goodleform button

--- src/Pages/Butnow/Buynow.js
+++ src/Pages/Butnow/Buynow.js
@@ -1,12 +1,20 @@
 import './Buynow.css';
 import imgz from './../../images/buy.png';
+import imgy from './../../images/googleplay.png';
+import imgx from './../../images/googleform.png';
 const Buynow=()=>{
     return(
         <>
 
     <div container="cot">
-     <img className="size" src={imgz}/>
+   <img  className="size" src={imgz}/>
+ 
+
     </div>
+    <div className="flex">
+  <img className="play" src={imgy}/>
+  <img className="form" src={imgx}/>
+  </div>
         </>
     )
 }
